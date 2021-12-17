34b15bdfa88abad54c9bba70a1678c6087a9fcb0
John Nguyen <j4nguyen134@gmail.com>
2021-12-17 11:05:18 -0600

    thread screen without constraints

--- EducateMates/EducateMates/FeedViewController.swift
+++ EducateMates/EducateMates/FeedViewController.swift
@@ -6,16 +6,53 @@
 //
 
 import UIKit
+import Parse
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var threads = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Threads")
+        query.includeKey("author")
+        
+        query.limit = 50
+        
+        query.findObjectsInBackground { (threads, error) in
+            if threads != nil {
+                self.threads = threads!
+                self.tableView.reloadData()
+            }
+        }
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return threads.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ThreadTableViewCell") as! ThreadTableViewCell
+        
+        let thread = threads[indexPath.row]
+        let user = thread["author"] as! PFUser
+        cell.authorLabel.text = user.username
+        cell.titleLabel.text = thread["title"] as! String
+        cell.threadTextView.text = thread["thread"] as! String
+        
+        return cell
+    }
     /*
     // MARK: - Navigation
 
